5240a55f17e5ca660e05707f77172bb16640cec3
Mityaechka <50111604+Mityaechka@users.noreply.github.com>
2020-03-18 13:56:48 +0300

    0

--- EducationQuiz/src/modules/quizzes/components/quiz-edit/quiz-edit.component.ts
+++ EducationQuiz/src/modules/quizzes/components/quiz-edit/quiz-edit.component.ts
@@ -38,12 +38,12 @@ export class QuizEditComponent implements OnInit {
       const id = params.id;
       this.quizService.getQuiz(id).subscribe((data) => {
         this.quiz = data;
-        console.log(this.quiz);
         let index = 0;
         this.quiz.questions.forEach((e) => {
           e.index = index;
           index++;
         });
+        console.log(this.quiz);
         this.setQuiz();
         if (this.quiz.questions.length === 0) {
           this.quiz.questions.push(new QuestionEdit());
@@ -53,28 +53,35 @@ export class QuizEditComponent implements OnInit {
     });
   }
   submit() {
+    this.getQuestion();
 
+    this.setQuestion();
+    console.log(this.quiz);
+    this.quizService.updateQuiz(this.quiz).subscribe((data) => {
+      console.log(data);
+    });
   }
   nextQuestion() {
-    if (!this.editForm.valid) {
+    /* if (!this.editForm.valid) {
       return;
-    }
+    } */
     this.getQuestion();
     this.index++;
     this.setQuestion();
   }
   previousQuestion() {
-    if (!this.editForm.valid) {
+    /* if (!this.editForm.valid) {
       return;
-    }
+    } */
     this.getQuestion();
     this.index--;
     this.setQuestion();
   }
   addQuestion() {
+    /* console.log(this.editForm.errors);
     if (!this.editForm.valid) {
       return;
-    }
+    } */
     this.getQuestion();
     this.quiz.questions.push(new QuestionEdit());
     this.index++;
@@ -86,27 +93,27 @@ export class QuizEditComponent implements OnInit {
     this.editForm.controls.description.setValue(this.quiz.description);
   }
   getQuestion() {
-    console.log('index:' + this.index);
     const q = this.quiz.questions[this.index];
     q.question_title = this.questionForm.controls.question_title.value;
-
+    q.answers = [];
     (this.questionForm.controls.answers as FormArray).controls.forEach(element => {
       const a = new AnswerEdit();
+      console.log((element as FormGroup).controls.id.value);
       a.index = (element as FormGroup).controls.index.value;
       a.id = (element as FormGroup).controls.id.value;
       a.answer_text = (element as FormGroup).controls.answer_text.value;
-      a.is_correct = (element as FormGroup).controls.id.value;
-      if (q.answers.find(x => (x.id != -1 && x.id === a.id) || x.index === a.index)) {
-        console.log('set');
+      a.is_correct = (element as FormGroup).controls.is_correct.value === true ? true : false;
+      /* if (q.answers.find(x => (x.id != -1 && x.id === a.id) || x.index === a.index)) {
         const i = q.answers.indexOf(q.answers.find(x => x.id === a.id && x.index === a.index));
         q.answers[i] = a;
       } else {
-        console.log('push');
         q.answers.push(a);
-      }
+      } */
+      q.answers.push(a);
     });
+    //console.log(q);
+    //console.log(this.quiz.questions[this.index]);
     this.quiz.questions[this.index] = q;
-    console.log(this.quiz);
     (this.questionForm.controls.answers as FormArray).clear();
   }
   setQuestion() {
@@ -127,7 +134,6 @@ export class QuizEditComponent implements OnInit {
     this.questionForm.controls.question_title.setValue(question.question_title);
     const answersForm = (this.questionForm.controls.answers as FormArray);
     answersForm.clear();
-    console.log(question.answers);
     question.answers.forEach(element => {
       answersForm.push(new FormGroup({
         index: new FormControl(element.index),
@@ -142,7 +148,6 @@ export class QuizEditComponent implements OnInit {
     const index = (this.questionForm.controls.answers as FormArray).length != 0 ?
       Math.max.apply(Math, (this.questionForm.controls.answers as FormArray).controls.map((o) => (o as FormGroup).controls.index.value)) + 1
       : 1;
-    console.log(index);
     (this.questionForm.controls.answers as FormArray).push(new FormGroup({
       index: new FormControl(index),
       id: new FormControl('-1'),

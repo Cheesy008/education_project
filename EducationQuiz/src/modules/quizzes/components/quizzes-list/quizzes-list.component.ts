a19e3b1eee4a28631a4b40bd6a513f542064f127
Mityaechka <50111604+Mityaechka@users.noreply.github.com>
2020-03-19 18:32:43 +0300

    Пагинация расширяемая

--- EducationQuiz/src/modules/quizzes/components/quizzes-list/quizzes-list.component.ts
+++ EducationQuiz/src/modules/quizzes/components/quizzes-list/quizzes-list.component.ts
@@ -1,23 +1,25 @@
-import { Component, OnInit } from '@angular/core';
-import { ActivatedRoute } from '@angular/router';
-import { QuizService } from 'src/service/quiz.service';
-import { Quiz } from 'src/models/quiz.model';
-import { PageEvent } from '@angular/material/paginator';
+import { Component, OnInit } from "@angular/core";
+import { ActivatedRoute } from "@angular/router";
+import { QuizService } from "src/service/quiz.service";
+import { Quiz } from "src/models/quiz.model";
+import { PageEvent } from "@angular/material/paginator";
 
 @Component({
-  selector: 'app-quizzes-list',
-  templateUrl: './quizzes-list.component.html',
-  styleUrls: ['./quizzes-list.component.scss']
+  selector: "app-quizzes-list",
+  templateUrl: "./quizzes-list.component.html",
+  styleUrls: ["./quizzes-list.component.scss"]
 })
 export class QuizzesListComponent implements OnInit {
   page: number;
   quizzes: Quiz[] = [];
   pageSize = 1;
   length = 1;
-  constructor(private route: ActivatedRoute, private quizService: QuizService) { }
+  constructor(
+    private route: ActivatedRoute,
+    private quizService: QuizService
+  ) {}
   pageEvent: PageEvent;
   ngOnInit(): void {
-
     this.route.params.subscribe((params: any) => {
       if (params.page) {
         this.page = params.page;
@@ -31,14 +33,13 @@ export class QuizzesListComponent implements OnInit {
     if (event) {
       this.page = event.pageIndex + 1;
     }
-    this.quizService.getQuizzes(this.page).subscribe((data) => {
+    this.quizService.getQuizzes(this.page).subscribe(data => {
       this.quizzes = [];
       this.length = data.count;
+      this.pageSize = data.results.length;
       data.results.forEach(element => {
         this.quizzes.push(Quiz.parse(element));
       });
-      console.log(this.quizzes);
     });
   }
 }
-

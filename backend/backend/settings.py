6cf8544d64dceacc8588944d6b87a7db8cd3ad39
Cheesy008 <mr.world008@gmail.com>
2020-03-19 14:15:46 +0600

    Changed pagination

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -65,7 +65,7 @@ REST_FRAMEWORK = {
         'api.disable.CsrfExemptSessionAuthentication',
     ],
     'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
-    'PAGE_SIZE': 1,
+    'PAGE_SIZE': 2,
 }
 
 MIDDLEWARE = [
